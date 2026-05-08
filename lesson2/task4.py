2114a16130c586d54fc2011623c0f5e075dd7229
stepan <stepan.hovorov@gmail.com>
2026-05-08 22:42:03 +0200

    Створіть програму, в якій користувач буде вказувати два числа — *з* і *по* яке число виводити числа. Наприклад: 5 і 10, тоді у відповідь мають вивестись всі числа від 5 до 10

---
+++ lesson2/task4.py
@@ -0,0 +1,13 @@
+
+
+number_from = int(input('What is your 1st number: '))
+number_to = int(input('What is your 2nd number: '))
+
+if number_from > number_to:
+    print('1st number need to be biger than 2nd number ')
+
+numberRonaldo = number_from
+while numberRonaldo <= number_to:
+    print(numberRonaldo)
+    numberRonaldo +=1
+
