31767c85e58f303269521979ab2fd4fe8f64a5dc
stepan <stepan.hovorov@gmail.com>
2026-05-08 22:57:29 +0200

    Створіть програму, яка буде виводити на екран, в рядок, тільки парні числа від 1 до n у зворотному порядку.

--- lesson2/task5.py
+++ lesson2/task5.py
@@ -1,9 +1,15 @@
 n = int(input('What your number: '))
 
 if 1 > n:
-    print('n number need to be biger 1 ')
+    print('n number need to be bigger 1 ')
 
+if n % 2 == 0:
 
-while 1 <= n:
-    print(n)
-    n -=2
+    while 1 <= n:
+        print(n)
+        n -=2
+else:
+    n -=1
+    while 1 <= n:
+        print(n)
+        n -=2
