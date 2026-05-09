6de5a77753e05c4c20678bfb8aab3185b7e493b6
stepan <stepan.hovorov@gmail.com>
2026-05-09 10:48:06 +0200

    marks

---
+++ lesson2/task7.py
@@ -0,0 +1,16 @@
+n = int(input('What your score: '))
+
+
+
+if n<=49:
+    print("незадовільно")
+
+if n>=50 and n<=69:
+    print("задовільно")
+
+if n>=70 and n<=89:
+    print("добре")
+
+if n>=90 and n<=100:
+    print("відмінно")
+
